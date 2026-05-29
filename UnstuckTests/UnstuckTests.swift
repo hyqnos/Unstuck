ad405f648cc93a8ca8fa51b15dcc3eec1d648dd4
Deniz Timbiloglu <deniztimbiloglu@gmail.com>
2026-05-30 00:32:55 +0200

    Initial Commit

---
+++ UnstuckTests/UnstuckTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnstuckTests.swift
+//  UnstuckTests
+//
+//  Created by MacBook on 30.05.2026.
+//
+
+import Testing
+@testable import Unstuck
+
+struct UnstuckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ UnstuckUITests/UnstuckUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  UnstuckUITestsLaunchTests.swift
+//  UnstuckUITests
+//
+//  Created by MacBook on 30.05.2026.
+//
+
+import XCTest
+
+final class UnstuckUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
